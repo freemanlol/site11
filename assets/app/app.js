ed12c3ec560b30c9eb487007e7117afba5aeea77
Freeman <48633960+freemanlol@users.noreply.github.com>
2023-07-31 11:26:07 +0300

    add changes

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-        
+    btnTeam = document.querySelectorAll("#btn__team");
+    btnTeam.forEach((userItem) => {
+    	userItem.addEventListener("click", () => {
+    		console.log(userItem)
+    		btnTeam.forEach((userItem) => {
+    			userItem.classList.remove("btn__active")
+    		});
+    		userItem.classList.add("btn__active")
+    	})
+	});
 });
 
 
