25d7bfcf8b82c30c079076bd03f9346fb29288cf
ilya-zabolotny <34621447+Acex187x@users.noreply.github.com>
2023-08-03 19:09:16 +0200

    Animated presentation line

--- assets/app/app.js
+++ assets/app/app.js
@@ -11,4 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 });
 
+// Presentation line animation
 
+// Length of path animated
+const LINE_LENGTH = 9902;
+
+document.addEventListener("scroll", (event) => {
+	const presentationOffset = document.querySelector('.presentation').offsetTop
+	const screenHeight = window.innerHeight;
+	const screenWidth = window.innerWidth;
+
+	const totalTopOffset = (presentationOffset - (screenHeight / 2))
+
+	// this coef is working for 1920x1080 screen
+	// TODO: make it work for all screen sizes
+	const coef = 0.95;
+
+	document.querySelector(".line-path").style.strokeDashoffset = LINE_LENGTH - (window.scrollY - totalTopOffset) * coef;
+
+});
+
+// Presentation line animation end
